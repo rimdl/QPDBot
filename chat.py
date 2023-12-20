8056c10e9561cd88e73aff36e7a0c4eb2ff54e5a
顾右 <57860092+rimdl@users.noreply.github.com>
2023-12-20 14:39:18 +0800

    Delete chat.py

--- chat.py
+++
@@ -1,9 +0,0 @@
-import AI.gemini as gemini
-
-
-def chat(content, config, gemini_settings):
-    if content['AI'] == 'gemini':
-        if content['image'] == "":
-            return gemini.chat_multi_turn(content['history'], config, gemini_settings)
-    else:
-        return "hgahah"
